353830eca7a7c491f9acd46f80a991afa1613940
Zakir Durumeric <zakird@gmail.com>
2013-10-03 18:40:32 -0400

    adding regular roles to sushi.py

--- sushi.py
+++ sushi.py
@@ -27,7 +27,26 @@ class Restaurant(object):
 
 nagomi = Restaurant("Nagomi", "...", [
 	RegularRoll("Roll", 5.34, "Tasty fish"),
-
+	RegularRoll("Cucumber", 2.50, None),
+	RegularRoll("Avocado", 2.50, None),
+	RegularRoll("Black Mushroom", 2.50, None),
+	RegularRoll("Asparagus", 2.75, None),
+	RegularRoll("Asparagus, Avocado, Cucumber", 2.75, None),
+	RegularRoll("Tuna", 3.50, None),
+	RegularRoll("Spicy Tuna", 4.50, None),
+	RegularRoll("Salmon", 3.50, None),
+	RegularRoll("Spicy Salmon", 4.50, None),
+	RegularRoll("Salmon Skin", 4.00, None),
+	RegularRoll("Yellow Tail & Scallion", 4.00, None),
+	RegularRoll("Spicy Yellow Tail", 4.50, None),
+	RegularRoll("California", 3.75, None),
+	RegularRoll("Shrimp California", 4.50, None),
+	RegularRoll("Special California", 4.50, None),
+	RegularRoll("Eel", 4.00, None),
+	RegularRoll("East", 5.00, None),
+	RegularRoll("Alaskan", 5.50, None),
+	RegularRoll("Boston", 5.00, None),
+	RegularRoll("Philly Roll", 5.00, None),
 	])
 
 
